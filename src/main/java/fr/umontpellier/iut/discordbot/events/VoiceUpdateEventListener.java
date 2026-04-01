16b8353cc0e704d24b78d11d4faceb93462b7a21
Bloomy <contact@bloomyindev.me>
2026-04-01 11:58:33 +0200

    Ajout de la gestion des logs de connexion et déconnexion dans les canaux vocaux

--- src/main/java/fr/umontpellier/iut/discordbot/events/VoiceUpdateEventListener.java
+++ src/main/java/fr/umontpellier/iut/discordbot/events/VoiceUpdateEventListener.java
@@ -2,8 +2,13 @@ package fr.umontpellier.iut.discordbot.events;
 
 import fr.umontpellier.iut.discordbot.Bot;
 import fr.umontpellier.iut.discordbot.lib.AbstractEventListener;
+import net.dv8tion.jda.api.components.section.Section;
+import net.dv8tion.jda.api.components.separator.Separator;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.components.container.Container;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -25,10 +30,42 @@ public class VoiceUpdateEventListener extends AbstractEventListener {
     private void onGuildVoiceJoin(@NotNull GuildVoiceUpdateEvent event) {
         Channel channel = Objects.requireNonNull(event.getChannelJoined());
         logger.info("\"{}\" has joined voice channel \"{}\"", event.getMember().getEffectiveName(), channel.getName());
+
+        sendVoiceLog(
+                Container.of(
+                        TextDisplay.of("# \uD83D\udd0A Connexion vocal"),
+                        Separator.createDivider(Separator.Spacing.SMALL),
+                        Section.of(
+                                Thumbnail.fromUrl(event.getMember().getUser().getEffectiveAvatarUrl()),
+                                TextDisplay.ofFormat("**%s** a rejoint le canal vocal **%s**", event.getMember().getAsMention(), channel.getAsMention())
+                        )
+                ).withAccentColor(0x00FF00)
+        );
     }
 
     private void onGuildVoiceLeave(@NotNull GuildVoiceUpdateEvent event) {
         Channel channel = Objects.requireNonNull(event.getChannelLeft());
         logger.info("\"{}\" has left voice channel \"{}\"", event.getMember().getEffectiveName(), channel.getName());
+
+        sendVoiceLog(
+                Container.of(
+                        TextDisplay.of("# \uD83D\uDD07 Connexion vocal"),
+                        Separator.createDivider(Separator.Spacing.SMALL),
+                        Section.of(
+                                Thumbnail.fromUrl(event.getMember().getUser().getEffectiveAvatarUrl()),
+                                TextDisplay.ofFormat("**%s** a quitté le canal vocal **%s**", event.getMember().getAsMention(), channel.getAsMention())
+                        )
+                ).withAccentColor(0xFF0000)
+        );
+    }
+
+    private void sendVoiceLog(Container container) {
+        String voiceChannelId = getBot().getConfig().get().getVoiceChannelId();
+        if (voiceChannelId == null || voiceChannelId.isBlank()) {
+            logger.warn("No voice log channel configured; skipping Discord log message");
+            return;
+        }
+
+        getBot().getLogSender().sendComponentToChannelId(voiceChannelId, container);
     }
 }
