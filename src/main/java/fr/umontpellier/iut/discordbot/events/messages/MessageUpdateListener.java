89c6f9b5a6ba0624704855368d6d2d80bb3cc45f
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-09-24 14:00:56 +0200

    fix: résolution des fausses alertes de modifications fantômes
    - Application d'une logique Fail-Safe pour ignorer le rafraîchissement des embeds par l'API Discord sur les anciens messages.
    - refactor: mise en conformité Single-Entry Single-Exit (SESE) et suppression des branchements conditionnels multiples dans MessageUpdateListener.
    - Suppression des commentaires en ligne pour un code source auto-porteur.
    
    Closes #16

--- src/main/java/fr/umontpellier/iut/discordbot/events/messages/MessageUpdateListener.java
+++ src/main/java/fr/umontpellier/iut/discordbot/events/messages/MessageUpdateListener.java
@@ -2,6 +2,8 @@ package fr.umontpellier.iut.discordbot.events.messages;
 
 import java.time.ZoneOffset;
 
+import javax.annotation.Nonnull;
+
 import fr.umontpellier.iut.discordbot.Bot;
 import fr.umontpellier.iut.discordbot.config.ConfigStructure.SystemChannel;
 import fr.umontpellier.iut.discordbot.lib.AbstractEventListener;
@@ -11,12 +13,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.utils.TimeFormat;
 
-/**
- * Journalise les modifications de message et met à jour le cache, pour que le log de suppression affiche la dernière
- * version.
- */
 public class MessageUpdateListener extends AbstractEventListener {
-	// Ancien et nouveau contenu partagent la limite de texte du log
 	private static final int MAX_CONTENT_LENGTH = 1700;
 
 	public MessageUpdateListener(Bot bot) {
@@ -24,40 +21,55 @@ public class MessageUpdateListener extends AbstractEventListener {
 	}
 
 	@Override
-	public void onMessageUpdate(MessageUpdateEvent event) {
-		if (!event.isFromGuild() || event.getAuthor().isBot()) {
-			return;
+	public void onMessageUpdate(@Nonnull MessageUpdateEvent event) {
+		boolean isValidSource = false;
+
+		if (event.isFromGuild()) {
+			if (!event.getAuthor().isBot()) {
+				isValidSource = true;
+			}
 		}
 
-		Message msg = event.getMessage();
-		String newContent = msg.getContentRaw();
+		if (isValidSource) {
+			Message msg = event.getMessage();
+			String newContent = msg.getContentRaw();
 
-		CachedMessage previous = getBot().getCachedMessages().put(event.getMessageId(), new CachedMessage(
-				event.getAuthor().getId(),
-				newContent,
-				msg.getTimeCreated().atZoneSameInstant(ZoneOffset.UTC)
-		));
+			CachedMessage previous = getBot().getCachedMessages().put(event.getMessageId(), new CachedMessage(
+					event.getAuthor().getId(),
+					newContent,
+					msg.getTimeCreated().atZoneSameInstant(ZoneOffset.UTC)));
 
-		// Discord envoie aussi cet événement quand seuls les embeds ou l'épinglage changent
-		if (previous != null && previous.getContent().equals(newContent)) {
-			return;
+			boolean hasContentChanged = false;
+
+			if (previous != null) {
+				if (!previous.getContent().equals(newContent)) {
+					hasContentChanged = true;
+				}
+			}
+
+			if (hasContentChanged) {
+				String before = "*Contenu inconnu : message absent du cache.*";
+
+				if (previous != null) {
+					before = quote(previous.getContent());
+				}
+
+				String details = String.format(
+						"Auteur: %s\nSalon: %s\nEnvoyé: %s\nModifié: %s\n[Aller au message](%s)\n\n**Avant**\n%s\n\n**Après**\n%s",
+						event.getAuthor().getAsMention(),
+						event.getChannel().getAsMention(),
+						TimeFormat.DATE_TIME_SHORT.format(msg.getTimeCreated()),
+						TimeFormat.DATE_TIME_SHORT.now(),
+						msg.getJumpUrl(),
+						before,
+						quote(newContent));
+
+				logger.info("Message {} from {} edited in \"{}\"", event.getMessageId(), event.getAuthor().getId(),
+						event.getChannel().getName());
+				getBot().getLogSender().sendLog(SystemChannel.MESSAGE_EDIT_CHANNEL, "# ✏️ Message modifié", 0xFFFF00,
+						details);
+			}
 		}
-
-		String before = previous != null
-				? quote(previous.getContent())
-				: "*Contenu inconnu : message absent du cache.*";
-
-		String details = String.format("Auteur: %s\nSalon: %s\nEnvoyé: %s\nModifié: %s\n[Aller au message](%s)\n\n**Avant**\n%s\n\n**Après**\n%s",
-				event.getAuthor().getAsMention(),
-				event.getChannel().getAsMention(),
-				TimeFormat.DATE_TIME_SHORT.format(msg.getTimeCreated()),
-				TimeFormat.DATE_TIME_SHORT.now(),
-				msg.getJumpUrl(),
-				before,
-				quote(newContent));
-
-		logger.info("Message {} from {} edited in \"{}\"", event.getMessageId(), event.getAuthor().getId(), event.getChannel().getName());
-		getBot().getLogSender().sendLog(SystemChannel.MESSAGE_EDIT_CHANNEL, "# ✏️ Message modifié", 0xFFFF00, details);
 	}
 
 	private static String quote(String content) {
